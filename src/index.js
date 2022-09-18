28858a8839e3b44f323f380376de6388f98af449
Wilson Chen <wilsonchen901@gmail.com>
2022-09-18 17:25:22 -0400

    start model canvas

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     var canvas = document.getElementById("model-canvas");
     var parent = document.getElementById("character-canvas");
+
     canvas.width = parent.offsetWidth;
     canvas.height = parent.offsetHeight;
+
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let x = 0;
+
+    function animate () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(x, 50, 100, 100);
+        x++;
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 })
