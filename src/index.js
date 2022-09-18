bca214124ad6465726a25d7d00b938196d7f0ebb
Wilson Chen <wilsonchen901@gmail.com>
2022-09-18 16:28:13 -0400

    setup canvas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    
+    console.log("hello world!")
+
     var canvas = document.getElementById("model-canvas");
-    var parent = document.getElementById("cnaracter-canvas");
+    var parent = document.getElementById("character-canvas");
     canvas.width = parent.offsetWidth;
     canvas.height = parent.offsetHeight;
 })
